b83fea53842f740b844c48dbc153d9da5fee1018
Tom Scott <tscott@stratanetworks.com>
2024-05-04 14:09:57 -0600

    handle schemes

--- swagger.js
+++ swagger.js
@@ -2,12 +2,21 @@ const dotenv = require("dotenv");
 dotenv.config();
 const swaggerAutogen = require("swagger-autogen")();
 
+const environment = process.env.NODE_ENV || "development";
+
+let schema = ["http"];
+
+if (environment === "production") {
+  schema = ["https"];
+}
+
 const doc = {
   info: {
     title: "Contacts API",
     description: "An API for Contacts",
   },
   host: process.env.API_URL,
+  schemes: schema,
 };
 
 const outputFile = "./swagger.json";
